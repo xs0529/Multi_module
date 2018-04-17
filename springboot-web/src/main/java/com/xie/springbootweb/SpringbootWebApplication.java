7d8070a9426f108410041fd437952a1d2289eadf
xs0529 <1312544013@qq.com>
2018-04-17 17:19:03 +0800

    多模块项目

---
+++ springboot-web/src/main/java/com/xie/springbootweb/SpringbootWebApplication.java
@@ -0,0 +1,14 @@
+package com.xie.springbootweb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.xie.springbootweb.controller","com.xie.service.impl"})
+@MapperScan("com.xie.springbootdao.dao")
+public class SpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebApplication.class, args);
+	}
+}
---
+++ springboot-web/src/main/java/com/xie/springbootweb/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.xie.springbootweb.controller;
+
+import com.xie.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author 谢霜
+ * @Description
+ * @date 2018/4/17 11:45
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "hello ni hao,wo shi xie shuang heh";
+    }
+    @RequestMapping("/hello1")
+    public String sayHello1(){
+        return "hello";
+    }
+    @RequestMapping("/users")
+    public String getUsers(){
+        return helloService.getUsers().toString()+"wuyu";
+    }
+    @RequestMapping("/user/{id}")
+    public String getUserById(@PathVariable(value = "id")Byte id){
+        return helloService.getUserById(id).toString();
+    }
+    @RequestMapping("/user")
+    public String getUser(){
+        return helloService.getUserById((byte) 1).toString();
+    }
+}
